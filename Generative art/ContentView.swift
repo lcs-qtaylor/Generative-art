516e64a9359ce1efa44a7b9e79174ffa0bdd1bb8
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-10-24 11:33:24 -0400

    Initial Commit

---
+++ Generative art/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Generative art
+//
+//  Created by Quin Taylor on 2023-10-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Generative art/Generative_artApp.swift
@@ -0,0 +1,17 @@
+//
+//  Generative_artApp.swift
+//  Generative art
+//
+//  Created by Quin Taylor on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct Generative_artApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
