93e2e4e3fdb330ce387fa4583d518e4f6253b121
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-10-24 11:37:13 -0400

    made square

--- Generative art/ContentView.swift
+++ Generative art/ContentView.swift
@@ -10,12 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           Rectangle()
+                .aspectRatio(1.0, contentMode: .fit)
         }
-        .padding()
+        .padding(100)
     }
 }
 
